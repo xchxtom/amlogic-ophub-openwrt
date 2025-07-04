292af1c17072c87b66e561766394db21e0778ca0
xchxtom <103757778+xchxtom@users.noreply.github.com>
2025-07-04 21:53:09 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -22,6 +22,57 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 
 # ------------------------------- Other started -------------------------------
 #
+wget -P feeds/packages/multimedia/minidlna/patches/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/main/002-support-ape-mka-format.patch
+#rm -f feeds/packages/sound/mpd/Makefile
+#wget -P feeds/packages/sound/mpd/ https://raw.githubusercontent.com/stock169/amlogic-s9xxx-openwrt/main/Makefile
+rm -f feeds/packages/lang/golang/golang/Makefile
+wget -P feeds/packages/lang/golang/golang/ https://raw.githubusercontent.com/sbwml/packages_lang_golang/refs/heads/24.x/golang/Makefile
+rm -f feeds/packages/utils/podman/Makefile
+wget -P feeds/packages/utils/podman/ https://raw.githubusercontent.com/stock169/openwrt-ipq40xx-generic-p2w_r619ac-128m/refs/heads/main/Makefile
+#rm -f target/linux/armsr/modules.mk
+#wget -P target/linux/armsr/ https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/target/linux/armsr/modules.mk
+sed -i 's/USER="mpd"/USER="root"/g' feeds/packages/sound/mpd/files/mpd.init
+sed -i 's/GROUP="mpd"/GROUP="root"/g' feeds/packages/sound/mpd/files/mpd.init
+sed -i 's/mkdir \$(PKG_BUILD_DIR)\/\$(ARCH)/mkdir -p \$(PKG_BUILD_DIR)\/\$(ARCH)/g' feeds/packages/utils/coremark/Makefile
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/.git
+rm -rf feeds/luci/applications/luci-app-passwall/.github
+mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/net/pdnsd-alt
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+rm -rf feeds/packages/net/trojan-plus
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/xray-plugin
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/shadowsocks-rust
+rm -rf feeds/packages/net/sing-box
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/tuic-client
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/ssocks
+rm -rf feeds/packages/net/trojan-go
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/brook
+rm -rf feeds/packages/net/dns2tcp
+rm -rf feeds/packages/net/geoview
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git feeds/packages/net/tmp
+rm -rf feeds/packages/net/tmp/.git
+rm -rf feeds/packages/net/tmp/.github
+rm -rf feeds/packages/net/tmp/gn
+mv feeds/packages/net/tmp/* feeds/packages/net
+# Add luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
